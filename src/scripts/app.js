a350e054b91a88052ab67935103f8c6851c12348
ninon28 <ninon.gossiaux@gmail.com>
2026-05-04 21:00:55 +0200

    js du slider

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,17 @@
 "use strict";
 
+//SLIDER
+const sliderContainer = document.querySelector('.sliderContainer');
+
+// mouvement de translation
+// on prend le sliderContainer et on modifie son transform -> la translationX = le scrollY en px
+function syncScroll() {
+  sliderContainer.style.transform = `translateX(${-window.scrollY}px)`;
+}
+// écouter le scroll
+window.addEventListener('scroll', syncScroll);
+
+//FOOTER
 var currentYear = new Date().getFullYear();
 var yearFooter = document.querySelector(".annee");
 yearFooter.textContent = currentYear
