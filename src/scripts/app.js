b8f102153959371f8d97bad42ef062e78b82ad60
ninon28 <ninon.gossiaux@gmail.com>
2026-05-13 12:49:49 +0200

    fonctionnement pour l'ouverture et la fermeture des sections

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -11,6 +11,33 @@ function syncScroll() {
 // écouter le scroll
 window.addEventListener('scroll', syncScroll);
 
+
+//SECTIONS CONTENT
+//comme slide--X = contentContainer--X -> on peut faire une boucle pour pas devoir écrire la fonction pour chaque heure
+//puis on récupère avec des doc.querySelector
+//on vérifie que ça existe pour pas tout bugger et on écoute le click + on add la classe open
+for (let i = 1; i <= 9; i++){
+    const slide = document.querySelector('.sliderContainer__slide--' + i);
+    const section = document.querySelector('.contentContainer--' + i);
+    if (slide && section){
+        slide.addEventListener('click', function() {
+            section.classList.add('open');
+            body.classList.add('no-scroll');
+            console.log("c'est booon")
+        });
+
+        //section et pas document.querySelector pour chercher que la croix qui est dans la section
+        const closeBtn = section.querySelector('.closeBtn');
+
+        if (closeBtn){
+            closeBtn.addEventListener('click', function() {
+                section.classList.remove('open');
+                body.classList.remove('no-scroll');
+            });
+        }
+    }
+}
+
 //FOOTER
 var currentYear = new Date().getFullYear();
 var yearFooter = document.querySelector(".annee");
